f8e42671219a61d80c8ce305f995a1c9837f256d
Asim-Shahriar <asimshariar@gmail.com>
2020-06-09 21:01:43 +0600

    callback interface method added

--- app/src/main/java/com/example/mvvm_practise_userlogin_registration/callback/LoginResultCallBack.java
+++ app/src/main/java/com/example/mvvm_practise_userlogin_registration/callback/LoginResultCallBack.java
@@ -2,4 +2,6 @@ package com.example.mvvm_practise_userlogin_registration.callback;
 
 public interface LoginResultCallBack {
 
+    void onSuccess(String message);
+    void onError(String message);
 }
