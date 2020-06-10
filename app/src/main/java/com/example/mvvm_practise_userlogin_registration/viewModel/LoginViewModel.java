dedbbe49126e04a2444fcd5a5cc9e6b090e3510f
Asim-Shahriar <asimshariar@gmail.com>
2020-06-10 21:57:28 +0600

    LOginViewModel textwatcher method added

--- app/src/main/java/com/example/mvvm_practise_userlogin_registration/viewModel/LoginViewModel.java
+++ app/src/main/java/com/example/mvvm_practise_userlogin_registration/viewModel/LoginViewModel.java
@@ -1,4 +1,62 @@
 package com.example.mvvm_practise_userlogin_registration.viewModel;
 
-public class LoginViewModel {
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+
+import androidx.lifecycle.ViewModel;
+
+import com.example.mvvm_practise_userlogin_registration.callback.LoginResultCallBack;
+import com.example.mvvm_practise_userlogin_registration.model.UserModel;
+
+public class LoginViewModel extends ViewModel {
+
+  private UserModel userModel;
+  private LoginResultCallBack loginResultCallBack;
+
+  public LoginViewModel(LoginResultCallBack loginResultCallBack){
+      this.loginResultCallBack=loginResultCallBack;
+      this.userModel=new UserModel();
+  }
+
+  public TextWatcher getEmailTextWatcher(){
+
+   return new TextWatcher() {
+       @Override
+       public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+       }
+
+       @Override
+       public void onTextChanged(CharSequence s, int start, int before, int count) {
+           Log.i("checkWatcherEmail", "Start: "+start+", "+"before: "+before+","+"count: "+count);
+       }
+
+       @Override
+       public void afterTextChanged(Editable s) {
+        userModel.setEmailId(s.toString());
+       }
+   };
+  }
+
+  public TextWatcher getPasswordWatcher(){
+
+      return new TextWatcher() {
+          @Override
+          public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+          }
+
+          @Override
+          public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+          }
+
+          @Override
+          public void afterTextChanged(Editable s) {
+           userModel.setPassword(s.toString());
+          }
+      };
+  }
+
 }
