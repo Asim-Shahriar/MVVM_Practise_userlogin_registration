640cc9e05997a2cbeb5ea1d801df1fce030679c1
Asim-Shahriar <asimshariar@gmail.com>
2020-06-12 03:53:51 +0600

    mainactivity loginviewmodel factory code added

--- app/src/main/java/com/example/mvvm_practise_userlogin_registration/MainActivity.java
+++ app/src/main/java/com/example/mvvm_practise_userlogin_registration/MainActivity.java
@@ -2,16 +2,39 @@ package com.example.mvvm_practise_userlogin_registration;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.example.mvvm_practise_userlogin_registration.callback.LoginResultCallBack;
 import com.example.mvvm_practise_userlogin_registration.databinding.ActivityMainBinding;
+import com.example.mvvm_practise_userlogin_registration.viewModel.LoginViewModel;
+import com.example.mvvm_practise_userlogin_registration.viewModel.LoginViewModelFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoginResultCallBack {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         ActivityMainBinding activityMainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        activityMainBinding.setViewModel(ViewModelProviders.of(this,new LoginViewModelFactory(this))
+
+        .get(LoginViewModel.class));
+
+    }
+
+    @Override
+    public void onSuccess(String message) {
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onError(String message) {
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/mvvm_practise_userlogin_registration/viewModel/LoginViewModelFactory.java
+++ app/src/main/java/com/example/mvvm_practise_userlogin_registration/viewModel/LoginViewModelFactory.java
@@ -1,7 +1,23 @@
 package com.example.mvvm_practise_userlogin_registration.viewModel;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.mvvm_practise_userlogin_registration.callback.LoginResultCallBack;
 
 public class LoginViewModelFactory extends ViewModelProvider.NewInstanceFactory {
 
+   private LoginResultCallBack loginResultCallBack;
+
+   public LoginViewModelFactory(LoginResultCallBack loginResultCallBack){
+
+     this.loginResultCallBack=loginResultCallBack;
+   }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return (T) new LoginViewModel(loginResultCallBack);
+    }
 }
